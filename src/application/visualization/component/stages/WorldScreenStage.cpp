35adff3efd9f7ecd9d2e176e8dab24de1fbe7c34
Moritz Willig <moritz.willig@gmail.com>
2017-06-25 01:13:03 +0200

    round texture synthesis button

--- src/application/visualization/component/stages/WorldScreenStage.cpp
+++ src/application/visualization/component/stages/WorldScreenStage.cpp
@@ -2,6 +2,8 @@
 // Created by moritz on 05.06.17.
 //
 
+#include <math.h>
+
 #include <glm/vec2.hpp>
 #include <overlay/OgreShapes.h>
 #include "WorldScreenStage.h"
@@ -12,15 +14,23 @@ namespace ape {
 
     WorldScreenStage::WorldScreenStage(AppWindow* appWindow):
         Stage(appWindow), overlay() {
+      std::vector<glm::vec2> circle;
+      auto circlePosX=0.875;
+      auto circlePosY=0.875;
+      auto r=0.10;
+      int numPts=20;
+
+      circle.emplace_back(circlePosX,circlePosY);
+      for (auto i=0; i<numPts+1; i++) {
+        circle.emplace_back(
+            circlePosX+(std::cos((i/(float)numPts)*M_PI*2.0)*r),
+            circlePosY+(std::sin((i/(float)numPts)*M_PI*2.0)*r)
+        );
+      }
+
       //texture synthesis button
       auto texSynth=std::make_shared<shapes::OgrePolygon2D>(
-          appWindow,std::vector<glm::vec2>({
-              glm::vec2(+0.85, +0.95),
-              glm::vec2(+0.95, +0.95),
-              glm::vec2(+0.95, +0.85),
-              glm::vec2(+0.85, +0.85)
-                                           }),
-          Ogre::ColourValue(0.8f,0.0f,0.0f));
+          appWindow, circle, Ogre::ColourValue(0.8f,0.0f,0.0f));
       overlay.childs.emplace_back(texSynth);
 
       setActive(false);
